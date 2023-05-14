75a3bc372e600e1d6ea6f3b37492b359759e031e
Anton Golub <antongolub@antongolub.com>
2023-05-14 21:30:40 +0300

    chore: mvn like dir layout

---
+++ src/scripts/build.cjs
@@ -0,0 +1,32 @@
+const esbuild = require('esbuild')
+const { nodeExternalsPlugin } = require('esbuild-node-externals')
+
+const esmConfig = {
+  entryPoints: ['./src/main/ts/index.ts'],
+  outdir: './target/esm',
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  sourcesContent: false,
+  platform: 'node',
+  target: 'ES2020',
+  format: 'esm',
+  plugins: [nodeExternalsPlugin()],
+  tsconfig: './tsconfig.json'
+}
+
+const cjsConfig = {
+  ...esmConfig,
+  outdir: './target/cjs',
+  platform: 'node',
+  target: 'es6',
+  format: 'cjs',
+}
+
+const config = process.argv.includes('--cjs')
+  ? cjsConfig
+  : esmConfig
+
+esbuild
+  .build(config)
+  .catch(() => process.exit(1))
---
+++ src/scripts/test.mjs
@@ -0,0 +1,5 @@
+import glob from 'fast-glob'
+
+const suites = await glob('src/test/**/*.test.{ts,cjs,mjs}', {cwd: process.cwd(), absolute: true, onlyFiles: true})
+
+await Promise.all(suites.map(suite => import(suite)))
